93bd33a4830d03a6ca70cafff59ea11eae60018b
Todd Warren <todd@warrenfamily.org>
2018-08-03 16:07:57 -0700

    Re-configure tests; add tests for VoterInfo

--- src/app/services/goog-civic.service.spec.ts
+++ src/app/services/goog-civic.service.spec.ts
@@ -4,11 +4,21 @@ import {
   HttpTestingController
 } from "@angular/common/http/testing";
 import { HttpClient, HttpErrorResponse } from "@angular/common/http";
+import {
+  CivicElectionRequest,
+  CivicElection,
+  APIKEY,
+  ELECTIONURL,
+  CivicVoterInfo,
+  VOTERURL,
+  DEFAULTADDRESS
+} from "../common/googcivic";
 import { GoogCivicService } from "./goog-civic.service";
-
+import { TESTDATA } from "./testdata";
 describe("GoogCivicService", () => {
   let httpClient: HttpClient;
   let httpTestingController: HttpTestingController;
+  let googService: GoogCivicService;
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
@@ -17,6 +27,7 @@ describe("GoogCivicService", () => {
     // Inject the http service and test controller for each test
     httpClient = TestBed.get(HttpClient);
     httpTestingController = TestBed.get(HttpTestingController);
+    googService = TestBed.get(GoogCivicService);
   });
 
   it("should be created", inject(
@@ -25,4 +36,67 @@ describe("GoogCivicService", () => {
       expect(service).toBeTruthy();
     }
   ));
+  describe("#getElections", () => {
+    let expectedElections: CivicElectionRequest;
+    beforeEach(() => {
+      googService = TestBed.get(GoogCivicService);
+      expectedElections = TESTDATA.expectedElections;
+    }); // before each #getElections
+    it("should return expected Elections (called once)", () => {
+      googService
+        .getElections()
+        .subscribe(
+          elects =>
+            expect(elects).toEqual(
+              expectedElections,
+              "should return expected elections"
+            ),
+          fail
+        );
+      const req = httpTestingController.expectOne(
+        r => r.method === "GET" && r.url === ELECTIONURL
+      );
+      expect(req.request.params.get("key")).toEqual(APIKEY);
+      req.flush(expectedElections);
+    });
+    it("elections should return a 404 error if thrown", () => {
+      const emsg = "deliberate 404 error";
+      googService.getElections().subscribe(
+        data => fail("should have failed with the 404 error"),
+        (error: HttpErrorResponse) => {
+          expect(error.status).toEqual(404, "status");
+          expect(error.error).toEqual(emsg, "message");
+        }
+      );
+      // Respond with mock error
+      const req = httpTestingController.expectOne(
+        r => r.method === "GET" && r.url === ELECTIONURL
+      );
+      req.flush(emsg, { status: 404, statusText: "Not Found" });
+    });
+  }); // describe #getElections
+  describe("#getVoterInfo", () => {
+    let expectedVoterInfo: CivicVoterInfo;
+    beforeEach(() => {
+      googService = TestBed.get(GoogCivicService);
+      expectedVoterInfo = TESTDATA.expectVoterInfo;
+    }); // before each
+    it("should return expected VoterInfo (called once)", () => {
+      googService
+        .getVoterInfo()
+        .subscribe(
+          vi =>
+            expect(vi).toEqual(
+              expectedVoterInfo,
+              "should return expected Voter Info"
+            ),
+          fail
+        );
+      const req = httpTestingController.expectOne(
+        r => r.method === "GET" && r.url === VOTERURL
+      );
+      expect(req.request.params.get("key")).toEqual(APIKEY);
+      expect(req.request.params.get("address")).toEqual(DEFAULTADDRESS);
+    });
+  }); // describe #getVoterInfo
 });
--- src/app/services/goog-civic.service.ts
+++ src/app/services/goog-civic.service.ts
@@ -1,13 +1,29 @@
 import { Injectable } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
-const CIVICURL = "https://www.googleapis.com/civicinfo/v2";
-const ELECTIONURL = CIVICURL + "elections";
-const VOTERURL = CIVICURL + "voterinfo";
-const APIKEY = "AIzaSyBrfCvcQtYQT4MQhuQ-m2HaGmYzxr49tVk";
+import { HttpClient, HttpParams } from "@angular/common/http";
+import { Observable } from "rxjs";
+import {
+  CivicElectionRequest,
+  CivicElection,
+  APIKEY,
+  ELECTIONURL,
+  CivicVoterInfo,
+  VOTERURL,
+  DEFAULTADDRESS
+} from "../common/googcivic";
 
 @Injectable({
   providedIn: "root"
 })
 export class GoogCivicService {
+  defaultParam = new HttpParams().set("key", APIKEY);
   constructor(private http: HttpClient) {}
+  getElections(): Observable<CivicElectionRequest> {
+    return this.http.get<CivicElectionRequest>(ELECTIONURL, {
+      params: this.defaultParam
+    });
+  }
+  getVoterInfo(address: string = DEFAULTADDRESS): Observable<CivicVoterInfo> {
+    const viParam = this.defaultParam.set("address", address);
+    return this.http.get<CivicVoterInfo>(VOTERURL, { params: viParam });
+  }
 }

3135f000648c76b1f24e311b44c80177d2261345
Todd Warren <todd@warrenfamily.org>
2018-08-03 07:20:22 -0700

    Create goog-civic service with bare bones http test and constants

--- src/app/services/goog-civic.service.spec.ts
+++ src/app/services/goog-civic.service.spec.ts
@@ -1,15 +1,28 @@
-import { TestBed, inject } from '@angular/core/testing';
+import { TestBed, inject } from "@angular/core/testing";
+import {
+  HttpClientTestingModule,
+  HttpTestingController
+} from "@angular/common/http/testing";
+import { HttpClient, HttpErrorResponse } from "@angular/common/http";
+import { GoogCivicService } from "./goog-civic.service";
 
-import { GoogCivicService } from './goog-civic.service';
-
-describe('GoogCivicService', () => {
+describe("GoogCivicService", () => {
+  let httpClient: HttpClient;
+  let httpTestingController: HttpTestingController;
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
       providers: [GoogCivicService]
     });
+    // Inject the http service and test controller for each test
+    httpClient = TestBed.get(HttpClient);
+    httpTestingController = TestBed.get(HttpTestingController);
   });
 
-  it('should be created', inject([GoogCivicService], (service: GoogCivicService) => {
-    expect(service).toBeTruthy();
-  }));
+  it("should be created", inject(
+    [GoogCivicService],
+    (service: GoogCivicService) => {
+      expect(service).toBeTruthy();
+    }
+  ));
 });
--- src/app/services/goog-civic.service.ts
+++ src/app/services/goog-civic.service.ts
@@ -1,9 +1,13 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+const CIVICURL = "https://www.googleapis.com/civicinfo/v2";
+const ELECTIONURL = CIVICURL + "elections";
+const VOTERURL = CIVICURL + "voterinfo";
+const APIKEY = "AIzaSyBrfCvcQtYQT4MQhuQ-m2HaGmYzxr49tVk";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class GoogCivicService {
-
-  constructor() { }
+  constructor(private http: HttpClient) {}
 }
